fa9b87cd1b385e99f91bab82c6d3fce320f603f5
wytreep <edwinacarabali@gmail.com>
2026-02-25 18:35:27 -0500

    fix Mejoras y gerarquia de admins

--- middleware.js
+++ middleware.js
@@ -1,26 +1,30 @@
 const jwt = require("jsonwebtoken")
-const SECRET = process.env.JWT_SECRET
+
 function verificarToken(req, res, next) {
     const token = req.headers["authorization"]
-
-    if (!token) {
-        return res.status(401).json({ error: "Acceso denegado, token requerido" })
-    }
+    if (!token) return res.status(401).json({ error: "Acceso denegado, token requerido" })
 
     try {
-        const decoded = jwt.verify(token, SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.usuario = decoded
         next()
     } catch (error) {
-        res.status(401).json({ error: "Token inválido o expirado" })
+        res.status(401).json({ error: "Token inválido" })
     }
 }
 
 function soloAdmin(req, res, next) {
-    if (req.usuario.rol !== "admin") {
-        return res.status(403).json({ error: "Acceso prohibido, se requiere rol de administrador" })
+    if (req.usuario.rol !== "admin" && req.usuario.rol !== "superadmin") {
+        return res.status(403).json({ error: "Acceso denegado" })
     }
     next()
 }
 
-module.exports = { verificarToken, soloAdmin }
+function soloSuperAdmin(req, res, next) {
+    if (req.usuario.rol !== "superadmin") {
+        return res.status(403).json({ error: "Solo el administrador principal puede hacer esto" })
+    }
+    next()
+}
+
+module.exports = { verificarToken, soloAdmin, soloSuperAdmin }
